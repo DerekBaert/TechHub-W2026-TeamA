f6151fb33155d83162e7e74db1f56190e58a5967
kalam-christopher <262621138+kalam-christopher@users.noreply.github.com>
2026-03-26 13:45:18 -0500

    enemy prefabs follow player now

--- Assets/TechHubPack_v2/Code/enemyMovement.cs
+++ Assets/TechHubPack_v2/Code/enemyMovement.cs
@@ -9,15 +9,18 @@ public class enemyMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+        player = GameObject.Find("/Player");
+    
+}
 
     // Update is called once per frame
     void Update()
     {
         if (followPlayer)
         {
+            //this figures out what direction to send the object which this is attached to
             mover = player.transform.position - transform.position;
+            //this tells the object this is attached to head that way
             transform.Translate(mover * Time.deltaTime);
         }
     }
