63ddee2a8822813304f58af3468d324fedd66361
kalam-christopher <262621138+kalam-christopher@users.noreply.github.com>
2026-04-28 14:52:21 -0500

    ranged enemy attacks still not quite working

--- Assets/scripts/health & damage/RangedEnemyAttack.cs
+++ Assets/scripts/health & damage/RangedEnemyAttack.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.RuleTile.TilingRuleOutput;
 
@@ -5,17 +6,28 @@ public class RangedEnemyAttack : MonoBehaviour
 {
     public GameObject enemyBullet;
     public GameObject player;
+    public int timeBetweenShots = 2;
+    public float time;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-        enemyBullet = Instantiate(enemyBullet, transform.position, transform.rotation);
-        enemyBullet.transform.LookAt(player.transform, Vector3.up);
+        GameObject.Find("Player");
+        GameObject.Find("EnemyBullet");
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        if (time > timeBetweenShots)
+        {
+            enemyBullet = Instantiate(enemyBullet, transform.position, transform.rotation);
+            enemyBullet.transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+            enemyBullet.transform.LookAt(player.transform, Vector3.up);
+            
+            time = 0;
+        }
+
     }
 }
