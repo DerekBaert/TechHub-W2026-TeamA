2d0d53199cd7fe19cf0f2e166d286b47eee6bc97
Krushie <Sprengerluke21@gmail.com>
2026-05-12 13:43:53 -0500

    bye bye death counter

--- Assets/scripts/DeathCounter.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Counter
-{
-
-    public void IncreaseCounter();
-
-}
--- Assets/scripts/Spawnertest.cs
+++ Assets/scripts/Spawnertest.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using System.Collections;
 
-public class Spawner : MonoBehaviour
+
+public class Spawnerlogic : MonoBehaviour
 {
+   private float xPosition;
+    private float yPosition;
+    private Vector3 spawnPosition;
+    [SerializeField]
+    private GameObject objectToSpawn;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        RandomSpawn();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void RandomSpawn()
+    {
+        xPosition = Random.Range(-45f, 45f);
+        yPosition = Random.Range(-45f, 45f);
+        spawnPosition = new Vector3(xPosition, yPosition, transform.position.z);
+        Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
     }
 }
