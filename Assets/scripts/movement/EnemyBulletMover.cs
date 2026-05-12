27bf64a50762292a2115198d4d6c904ebe7ca96e
kalam-christopher <262621138+kalam-christopher@users.noreply.github.com>
2026-05-12 13:22:56 -0500

    enemy bullet

--- Assets/scripts/movement/EnemyBulletMover.cs
+++ Assets/scripts/movement/EnemyBulletMover.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class EnemyBulletMover : MonoBehaviour
 {
+    public GameObject player;
+    private Vector2 direction;
+    public float speed = 5;
+    public int damage = 5;
+    public Health health;
+    private float time = 0f;
+    public float duration = 10f;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            health = player.GetComponent<Health>();
+            health.TakeDamage(damage);
+            Destroy(this.gameObject);
+        }  
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GameObject.Find("Player");
+        direction = player.transform.position - transform.position;
+        direction = Vector2.Normalize(direction);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       transform.Translate(direction * speed * Time.deltaTime);
+        time += Time.deltaTime;
+        if (time > duration)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
